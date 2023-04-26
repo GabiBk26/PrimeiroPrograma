0a9492980e5a1044a24e30f195e31452fecfb9c8
GabiBk26 <gabrielibu26@gmail.com>
2023-04-25 22:00:58 -0300

    Criando páginas com handlebars

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const http = require('http')
+const express = require("express");
+const app = express();
+const path=require ('path');//endereço de cada rota
+const router=express.Router();//trabalha com as rotas
 
-const hostname = '127.00.01';// Endereço do servidor
-const port = 3000; // porta do servidor
+const {engine} = require("express-handlebars");
 
-const server = http.createServer((req, res )=> {// criando o servidor
-   res.statusCode = 200; // status ok de empresa
-
-   res.setHeader ('content-type', 'text/plain ');
-   res.end('ola Meu nome GabiBk26');
-   
-});
-
-server.listen(port,hostname,()=>{
-    console.log(`servidor rodando em http://${hostname};${port}/`)
+app.engine('handlebars', engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
+        allowedProtoPropertiesByDefault: true,
+        allowedProtoMethodsByDefault: true,
+    },
+}))
+app.set('view engine', 'handlebars');
+router.get('/',function(req, res){
+    res.render('index');
+})
+router.get('/about',function(req,res){
+    res.sendFile(path.join(_dirname+'/about.html'));
 })
